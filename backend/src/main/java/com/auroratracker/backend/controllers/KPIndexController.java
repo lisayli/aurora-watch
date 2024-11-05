5006b34db628474dc9039ca55d53029a45e992d5
lisyli2 <lisa.ylikangas@hotmail.com>
2024-11-05 10:35:06 +0100

    Added Solarwind and Kp data with servcie and controller

---
+++ backend/src/main/java/com/auroratracker/backend/controllers/KPIndexController.java
@@ -0,0 +1,37 @@
+package com.auroratracker.backend.controllers;
+
+import com.auroratracker.backend.models.space.weatherfactors.KPIndex;
+import com.auroratracker.backend.services.KPIndexService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/aurora")
+public class SpaceWeatherController {
+
+    private final KPIndexService kpIndexService;
+
+    @Autowired
+    public SpaceWeatherController(KPIndexService kpIndexService) {
+        this.kpIndexService = kpIndexService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<String> getKpIndex() {
+        Optional<KPIndex> kpIndexOptional = kpIndexService.fetchLatestKpIndex();
+
+        if (kpIndexOptional.isPresent()) {
+            KPIndex kpIndex = kpIndexOptional.get();
+            return ResponseEntity.ok(kpIndex.toString());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Could not retrieve KP Index data.");
+        }
+    }
+}
